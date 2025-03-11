6b883d5e9893ccec53df0ba343ca04d53700d0da
MUSA UYUMAZ <musa.uyumaz73@gmail.com>
2025-03-11 19:48:41 +0300

    Odev yapildi

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,11 @@
+﻿//CTRL + K + S
+using ConsoleApp2.ErisimBelirleyiciler;
+
+#region Erisim Belirleyiciler
+User user = new();
+user.AdSoyad = "Fatih Bozkurt";
+user.Yas = 21;
+user.Email = "fatihbozkurt@gmail.com";
+
+user.BilgileriGoster();
+#endregion
