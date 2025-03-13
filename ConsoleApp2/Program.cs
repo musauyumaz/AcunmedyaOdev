123f3900340993f09f15dc44523da428f5c8fe1e
MUSA UYUMAZ <musa.uyumaz73@gmail.com>
2025-03-13 20:07:19 +0300

    Added to Arrays and Loops homework and Guess Number Game

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,4 +1,4 @@
-﻿//CTRL + K + S
+﻿
 using ConsoleApp2.ErisimBelirleyiciler;
 using MyNamespace;
 
@@ -12,9 +12,59 @@ user.BilgileriGoster();
 #endregion
 #region Namespace
 Hesaplama hesaplama = new();
-hesaplama.Topla(3,5);
-hesaplama.Carp(3,5);
+hesaplama.Topla(3, 5);
+hesaplama.Carp(3, 5);
 #endregion
 #region MerhabaDünya
 Console.WriteLine("Merhaba Dünya!");
+#endregion
+#region Sayı Tahmin
+
+Random random = new();
+long number = random.Next(0, 100);
+
+int guessNumber = -1;
+while (true)
+{
+    Console.Write("Sayı Giriniz :");
+    guessNumber = int.Parse(Console.ReadLine());
+
+    if (guessNumber < number)
+    {
+        Console.WriteLine("Yukarı");
+    }
+    else if (guessNumber > number)
+    {
+        Console.WriteLine("Aşağı");
+    }
+    else
+    {
+        Console.WriteLine("Sayıyı Doğru tahmin ettiniz.");
+        break;
+    }
+}
+
+#endregion
+#region Diziler Ve Döngüler
+
+int[] sayilar = new int[5];
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine(i + 1 + ". Sayı Giriniz: ");
+    if (int.TryParse(Console.ReadLine(), out int sayi))
+        sayilar[i] = sayi;
+    else
+        i -= 1;
+}
+
+int toplam = 0;
+foreach (var item in sayilar)
+{
+    toplam += item;
+}
+
+Console.WriteLine(toplam);
+Console.WriteLine(toplam / sayilar.Length);
+
 #endregion
